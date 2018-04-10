24ccde3c9606504658c3166ab324101273a66db5
David Hessler <davidh.092705@gmail.com>
2018-04-10 15:11:23 -0400

    Working Jar.  Fixed default values

--- src/main/java/cms/sre/gitlab_webhook/App.java
+++ src/main/java/cms/sre/gitlab_webhook/App.java
@@ -2,6 +2,8 @@ package cms.sre.gitlab_webhook;
 
 import cms.sre.mongo_connection_helper.MongoClientFactory;
 import cms.sre.mongo_connection_helper.MongoClientParameters;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -15,34 +17,34 @@ import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 @SpringBootApplication()
 public class App extends AbstractMongoConfiguration{
 
-    @Value("${mongodb.databaseName:null}")
+    @Value("${mongodb.databaseName:#{null}}")
     private String mongoDatabaseName;
 
-    @Value("${mongodb.keyStoreKeyPassword:null}")
+    @Value("${mongodb.keyStoreKeyPassword:#{null}}")
     private String mongoKeyStoreKeyPassword;
 
-    @Value("${mongodb.keyStoreLocation:null}")
+    @Value("${mongodb.keyStoreLocation:#{null}}")
     private String mongoKeyStoreLocation;
 
-    @Value("${mongodb.keyStorePassword:null}")
+    @Value("${mongodb.keyStorePassword:#{null}}")
     private String mongoKeyStorePassword;
 
-    @Value("${mongodb.trustStoreLocation:null}")
+    @Value("${mongodb.trustStoreLocation:#{null}}")
     private String mongoTrustStoreLocation;
 
-    @Value("${mongodb.trustStorePassword:null}")
+    @Value("${mongodb.trustStorePassword:#{null}}")
     private String mongoTrustStorePassword;
 
-    @Value("${mongodb.username:null}")
+    @Value("${mongodb.username:#{null}}")
     private String mongoUsername;
 
-    @Value("${mongodb.password:null}")
+    @Value("${mongodb.password:#{null}}")
     private String mongoPassword;
 
-    @Value("${mongodb.replicaSetLocation:null}")
+    @Value("${mongodb.replicaSetLocation:#{null}}")
     private String[] mongoReplicaSetLocation;
 
-    @Value("${mongodb.mongoReplicaSetName:null}")
+    @Value("${mongodb.mongoReplicaSetName:#{null}}")
     private String mongoReplicaSetName;
 
     @Value("${gitlab.classification:UNKNOWN}")
@@ -53,8 +55,43 @@ public class App extends AbstractMongoConfiguration{
         return this.gitlabClassification;
     }
 
+    private static String nullWrapper(String value){
+        return value == null ? "(null)" : value;
+    }
+
+    private static String nullWrapper(String[] values){
+        String ret = null;
+        if(values.length > 0){
+            ret = "";
+            for (String value : values) {
+                ret += nullWrapper(value);
+            }
+        }
+        return ret;
+    }
+
     @Override
     public MongoClient mongoClient() {
+        System.out.println();
+        System.out.println();
+        System.out.println("########################");
+
+        System.out.println("Configuration Values");
+        System.out.println("MongoKeyStoreKeyPassword : " + nullWrapper(this.mongoKeyStoreKeyPassword));
+        System.out.println("MongoKeyStorePassword : " + nullWrapper(this.mongoKeyStorePassword));
+        System.out.println("MongoKeyStoreLocation : " + nullWrapper(this.mongoKeyStoreLocation));
+        System.out.println("MongoTrustStoreLocation : " + nullWrapper(this.mongoTrustStoreLocation));
+        System.out.println("MongoTrustStorePassword : " + nullWrapper(this.mongoTrustStorePassword));
+        System.out.println("MongoDatabaseName : " + nullWrapper(this.mongoDatabaseName));
+        System.out.println("MongoUsername : " + nullWrapper(this.mongoUsername));
+        System.out.println("MongoPassword : " + nullWrapper(this.mongoPassword));
+        System.out.println("MongoReplicaSetLocation : " + nullWrapper(this.mongoReplicaSetLocation));
+        System.out.println("MongoReplicaSetName : " + nullWrapper(this.mongoReplicaSetName));
+
+        System.out.println("########################");
+        System.out.println();
+        System.out.println();
+
         MongoClientParameters params = new MongoClientParameters()
                 .setKeyStoreKeyPassword(this.mongoKeyStoreKeyPassword)
                 .setKeyStoreLocation(this.mongoKeyStoreLocation)
@@ -67,8 +104,20 @@ public class App extends AbstractMongoConfiguration{
                 .setReplicaSetLocations(this.mongoReplicaSetLocation)
                 .setReplicaSetName(this.mongoReplicaSetName);
 
-//        return MongoClientFactory.getLocalhostMongoClient();
-        return MongoClientFactory.getMongoClient(params);
+        MongoClient client = null;
+        if(this.mongoReplicaSetLocation.length == 1 && this.mongoReplicaSetLocation[0].equalsIgnoreCase("localhost")){
+            System.out.println("Connecting to Local Mongo");
+            if(this.mongoUsername != null && this.mongoUsername.length() > 0 && this.mongoPassword != null && this.mongoPassword.length() > 0){
+                client = MongoClientFactory.getLocalhostMongoClient(this.mongoDatabaseName, this.mongoUsername, this.mongoPassword);
+            }else{
+                client = MongoClientFactory.getLocalhostMongoClient();
+            }
+        }else{
+            System.out.println("Connecting to Remote Mongo");
+
+            client = MongoClientFactory.getMongoClient(params);
+        }
+        return client;
     }
 
     @Override
