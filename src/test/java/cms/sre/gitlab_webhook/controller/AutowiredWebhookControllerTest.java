bd1258939cbdb59a17bc6b90b36be6b87b92a609
edward.moffatt@markpointtech.com <edward.moffatt@markpointtech.com>
2018-07-27 12:30:29 +0000

    Added Wiremock for unit test to proper return from rest call. Updated .gitignore

--- src/test/java/cms/sre/gitlab_webhook/controller/AutowiredWebhookControllerTest.java
+++ src/test/java/cms/sre/gitlab_webhook/controller/AutowiredWebhookControllerTest.java
@@ -1,8 +1,11 @@
 package cms.sre.gitlab_webhook.controller;
 
 import cms.sre.gitlab_webhook.App;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -19,9 +22,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 @SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 public class AutowiredWebhookControllerTest {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8088); // No-args constructor defaults to port 8080
 
     @LocalServerPort
     private int port;
@@ -66,28 +76,48 @@ public class AutowiredWebhookControllerTest {
     }
 
     @Test
-    public void getGitlabPushEvent(){
+    public void getGitlabPushEvent() {
 
-        logger.debug("Request: "  + stringBuilder.toString());
+        logger.debug("Request: " + stringBuilder.toString());
 
-        String response = this.testRestTemplate.postForEntity("http://localhost:"+this.port+"/gitlabPushEvent", stringBuilder.toString(), String.class)
-                .getBody();
+        stubFor(post(urlEqualTo("/terraform/apply")).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json"))
+                .withRequestBody(equalToJson("{" +
+                              "\"user_email\": \"john@example.com\"," +
+                              "\"ssl_url\": \"git@example.com:mike/diaspora.git\"," +
+                              "\"revision_number\": \"da1560886d4f094c3e6c9ef40349f7d38b5d27d7\"," +
+                              "\"user_name\": \"John Smith\", \"pull_number\": \"95790bf891e76fee5e1747ab589903a6a1f80f22\"," +
+                              "\"branch_name\": \"refs/heads/master\", \"project_name\": \"docker-jenkins\"," +
+                              "\"classification\": \"WHATEVER\"," +
+                              "\"object_kind\": \"push\", \"timestamp\": \"Tue Jan 03 23:36:29 UTC 2012\"" +
+                              "}"))
+                .withRequestBody(notMatching("<status>ERROR</status>"))
+                .willReturn(aResponse().withStatus(200)));
 
-        Assertions.assertThat(response)
-                .isNotNull()
-                .isNotEmpty()
-                .contains("{\"object_kind\":\"push\",\"before\":\"95790bf891e76fee5e1747ab589903a6a1f80f22\",\"after\":\"da1560886d4f094c3e6c9ef40349f7d38b5d27d7\"," +
-                        "\"ref\":\"refs/heads/master\",\"checkout_sha\":null,\"user_id\":4,\"user_name\":\"John Smith\"," +
-                        "\"user_username\":null,\"user_email\":\"john@example.com\",\"user_avatar\":null,\"project_id\":15," +
-                        "\"project\":null,\"repository\":{\"name\":\"docker-jenkins\",\"url\":\"git@example.com:mike/diasporadiaspora.git\"," +
-                        "\"description\":\"\",\"homepage\":\"http://example.com/mike/diaspora\",\"git_http_url\":\"http://example.com/mike/diaspora.git\"," +
-                        "\"git_ssh_url\":\"git@example.com:mike/diaspora.git\",\"visibility_level\":0},\"commits\":[{\"id\":\"b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327\"," +
-                        "\"message\":\"Update Catalan translation to e38cb41.\",\"timestamp\":\"2011-12-12T14:27:31+02:00\"," +
-                        "\"url\":\"http://example.com/mike/diaspora/commit/b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327\",\"author\":{\"name\":\"Joe Schmoe\"," +
-                        "\"email\":\"Joe Schmoe\"},\"added\":null,\"modified\":null,\"removed\":null},{\"id\":\"da1560886d4f094c3e6c9ef40349f7d38b5d27d7\"," +
-                        "\"message\":\"fixed readme\",\"timestamp\":\"2012-01-03T23:36:29+02:00\"," +
-                        "\"url\":\"http://example.com/mike/diaspora/commit/da1560886d4f094c3e6c9ef40349f7d38b5d27d7\",\"author\":{\"name\":\"GitLab dev user\"," +
-                        "\"email\":\"gitlabdev@dv6700.(none)\"},\"added\":null,\"modified\":null,\"removed\":null}],\"total_commits_count\":4}");
+        int statusCodeValue = this.testRestTemplate.postForEntity("http://localhost:" + this.port + "/gitlabPushEvent", stringBuilder.toString(), String.class).getStatusCodeValue();
+
+        Assert.assertEquals(200, statusCodeValue);
+    }
+
+    @Test
+    public void exactUrlOnly() {
+
+        stubFor(post(urlEqualTo("/terraform/apply"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("")));
+
+        assertThat(this.testRestTemplate.postForEntity("http://localhost:" + this.port + "/gitlabPushEvent", stringBuilder.toString(), String.class).getStatusCodeValue(), is(200));
+
+    }
+
+    @Test
+    public void bodyMatching() {
+        stubFor(post(urlEqualTo("/terraform/apply")).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json"))
+                .withRequestBody(matching("<status>OK</status>"))
+                .withRequestBody(notMatching("<status>ERROR</status>"))
+                .willReturn(aResponse().withStatus(200)));
     }
 
 }
